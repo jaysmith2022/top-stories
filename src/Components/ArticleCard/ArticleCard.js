13ff92fdc94bd36d642eb48e454b6f5477bed0e5
Hazel Pablo <hazel.pablo90@yahoo.com>
2023-01-09 16:44:31 -0700

    display articles with links

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function ArticleCard() {
+function ArticleCard({ id, title, multimedia, section, url }) {
+  
   return (
-    <div>ArticleCard</div>
-  )
+    <div className="article-card">
+      <h2>{title}</h2>
+      <img
+        className="article-image"
+        alt={`image for ${title}`}
+        src={multimedia[1].url}
+        onError={(e) => (e.target.src = "/NYT-emblem.jpeg")}
+      />
+      <a href={url}>
+        <button className="card-btn" >
+          Link To Article
+        </button>
+      </a>
+      <button className="card-btn">Learn More Here</button>
+    </div>
+  );
 }
 
-export default ArticleCard
+export default ArticleCard;
--- src/Components/ArticleContainer/ArticleContainer.js
+++ src/Components/ArticleContainer/ArticleContainer.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ArticleCard from "../ArticleCard/ArticleCard";
 
-function ArticleContainer({articles}) {
- 
+function ArticleContainer({ articles }) {
   const displayArticles = articles.map((article) => {
-    return <ArticleCard key={article.id} id={article.id}/>;
+    return (
+      <ArticleCard
+        key={article.id}
+        id={article.id}
+        title={article.title}
+        multimedia={article.multimedia}
+        section={article.section}
+        url={article.url}
+      />
+    );
   });
-  
-  
-  return <div className="articles-container"><h1>Articles</h1>{displayArticles}</div>;
+
+  return (
+    <div className="articles-container">
+      
+      {displayArticles}
+    </div>
+  );
 }
 
 export default ArticleContainer;
